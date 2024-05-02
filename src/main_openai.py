0b38760fe11630b9af7ed3cc9bb3be9cb07db03f
Sangopak <sangojumech07@gmail.com>
2024-05-02 17:35:02 +0000

    Refactored the code

--- src/main_openai.py
+++ src/main_openai.py
@@ -1,27 +1,55 @@
 import json
 from openai import OpenAI
 import pandas as pd
+from pandas import DataFrame
 
-# Set up OpenAI API key
-my_assigned_team = 'team_13'
-keys = json.load(open('C:/shared/content/config/api-keys/hackathon_openai_keys.json'))
-my_key = keys[my_assigned_team]
+TEAM_KEY='team_13'
+CSV_DATA='portfolio_rebalancing_data.csv'
+MODEL='gpt-3.5-turbo-instruct'
 
-client = OpenAI(api_key=my_key)
+def setup_openai_client(my_assigned_team: str): 
+  # Set up OpenAI API key
+  #my_assigned_team = 'team_13'
+  keys = json.load(open('C:/shared/content/config/api-keys/hackathon_openai_keys.json'))
+  my_key = keys[my_assigned_team]
+  client = OpenAI(api_key=my_key)
+  return client
 
-# Convert the data to a Pandas DataFrame
-transactions_df = pd.read_csv('portfolio_rebalancing_data.csv')
+def read_training_data(csv_name: str):
+  # Convert the data to a Pandas DataFrame
+  transactions_df = pd.read_csv(csv_name)
+  return transactions_df
 
-# Convert DataFrame to a formatted string
-transactions_str = transactions_df.to_string(index=False)
+def convert_dataframe_to_string(transactions_df: DataFrame):
+  # Convert DataFrame to a formatted string
+  transactions_str = transactions_df.to_string(index=False)
+  return transactions_str
 
-prompt = "Given the portfolio transaction history:\n" + transactions_str + "\nDertmine the target portfolio allocation and analyze the rebalance trends from this data."
+def create_prompt(transactions_str: str):
+ return "Given the portfolio transaction history:\n" + transactions_str + "\nDertmine the target portfolio allocation and analyze the rebalance trends from this data."
 
-response = client.completions.create(
-  model="gpt-3.5-turbo-instruct",
-  prompt=prompt,
-  max_tokens=1024,
-  temperature=0.9
-)
+def call_openei_and_send_response(client, model, prompt):
+  # OpenAI with given prompt and model
+  response = client.completions.create(
+    model=model,
+    prompt=prompt,
+    max_tokens=1024,
+    temperature=0.9
+  )
+  return response.choices[0].text
 
-print(response.choices[0].text)
+def handler(team, csv_data, model):
+  # Create the openai client
+  openai_client = setup_openai_client(team)
+  # Read CSV data
+  transaction_dataframe = read_training_data(csv_data)
+  # Convert dataframe to string
+  transactions_str = convert_dataframe_to_string(transaction_dataframe)
+  # Create prompt
+  prompt = create_prompt(transactions_str)
+  # Call OpenAi to get analysis
+  print(f"Calling OpenAI for analysis for input {csv_data} with model {model}")
+  print(call_openei_and_send_response(openai_client, model, prompt))
+
+# Calling Handler
+handler(TEAM_KEY, CSV_DATA, MODEL)
